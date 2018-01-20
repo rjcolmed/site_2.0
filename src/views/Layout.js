eb3e8ebce22abbade47103486e70bcdb39213f0d
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-01-19 22:38:17 -0500

    fix nav menu links

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -4,7 +4,7 @@ const Layout = {
   view: vnode  => {
     return m("main.layout", [
       m("nav.menu", [
-        m("a[href='/'", { oncreate: m.route.link }, "Home")
+        m("a[href='/']", { oncreate: m.route.link }, "Home")
       ]),
       m("section", vnode.children),
       m("footer", [
