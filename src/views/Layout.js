0c2d4a41d6a186980d09d01219aefa55fcb7c963
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-01-20 11:28:51 -0500

    import and use Nav, Footer components

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,15 +1,13 @@
 import m from 'mithril'
+import Footer from '../components/Footer'
+import Nav from '../components/Nav'
 
 const Layout = {
   view: vnode  => {
     return m("main.layout", [
-      m("nav.menu", [
-        m("a[href='/']", { oncreate: m.route.link }, "Home")
-      ]),
+      m(Nav),
       m("section", vnode.children),
-      m("footer", [
-        m("p", "This is the footer...")
-      ])
+      m(Footer)
     ])
   }
 }
